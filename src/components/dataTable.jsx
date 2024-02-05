7919a89f88c24297b470706d35a705f0017a5396
Geetanjali <geetakorgaonkar93@gmail.com>
2024-02-06 03:32:10 +0530

    sort table with price and ticker

--- src/components/dataTable.jsx
+++ src/components/dataTable.jsx
@@ -1,15 +1,18 @@
-import React, {memo} from "react";
+import React, {memo, useState} from "react";
 
 const DataTable = ({tData}) => {
+    const [sortBy, setSortBy] = useState('');
+    const [tableData, setTableData] = useState(tData);
+
     // get table column
-    const column = Object.keys(tData[0]);
+    const column = Object.keys(tableData[0]);
 
     // get table heading data
     const ThData = () => (column.map((item)=> <th key={item} className="px-6 py-3">{item}</th> ))
 
     // get table row data
     const rowData = () =>(
-        tData.map((item) => (
+        tableData.map((item) => (
             <tr className={`border-b dark:border-gray-700 ${item.assetClass === 'Credit' ? 'bg-green-500' : item.assetClass === 'Equities' ? 'bg-blue-500' : 'bg-white-500'}`}>
                 {
                     column.map((v) => <td className={`px-6 py-4 ${v === 'price' && Math.sign(item[v]) === -1 ? 'text-red-800 font-medium' : 'text-black-400'}`}>{item[v]}</td>)
@@ -18,16 +21,49 @@ const DataTable = ({tData}) => {
         ))
     )
 
+    const handleChange = (e) => {
+        const {value} = e.target;
+        setSortBy(value);
+        if(value === 'price') {
+            const tempData = tData.toSorted((a, b) => b.price - a.price);
+            setTableData(tempData);
+        } else if(value === 'ticker') {
+            const tempData = tData.toSorted((a, b) => a.ticker < b.ticker ? -1 : 1);
+            setTableData(tempData);
+        } else if(value === 'assetClass') {
+            
+        }
+    }
+
     return(
         <>
-            <table className="w-full text-sm text-left rtl:text-right dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>{ThData()}</tr>
-                </thead>
-                <tbody>
-                    {rowData()}
-                </tbody>
-            </table>
+            <div className="py-4">
+                <label>SortBy: </label>
+                <select
+                    value={sortBy}
+                    onChange={handleChange}
+                    className="capitalize"
+                >
+                    <option value="" disabled>Select one</option>
+                    {
+                        column.map((col, i) => {
+                            return <option value={col} key={i} className="capitalize">{col}</option>
+                        })
+                    }
+                </select>
+            </div>
+            {
+                tableData.length ?
+                    <table className="w-full text-sm text-left rtl:text-right dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>{ThData()}</tr>
+                        </thead>
+                        <tbody>
+                            {rowData()}
+                        </tbody>
+                    </table>
+                : null
+            }
         </>
     )
 }
